528ee1e287c43fedbdfc56dc7ea69d7bff4b727d
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2025-09-22 17:15:30 +0530

    Add: different routes to serve different things
    the paths should be in such a way that INCOMING_REQ_OBJ will go from top to bottom manner
    
    if you put '/' path first then all the other routes like '/message' or '/add-product' are going to send the content of PATH '/' , cause that is the path which have been matched during top to bottom , so carefully place your path's content

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ const PORT = 3000;
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the MiddleWare!");
-    /* We have to tell that go to next middleware*/
-    next(); /* Allows request object to continue to next middleware */
-});
-
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
     console.log("Another Middleware");
+    console.log(req.url);
     res.setHeader("Connection", "keep-alive");
     res.send("<h1>HI From Express.js</h1>");
+});
+
+app.use("/message", (req, res, next) => {
+    console.log("inside the /message path");
+    console.log(req.url);
+    res.send(` inside the path of ${req.url}`);
+});
+
+app.use("/", (req, res, next) => {
+    console.log("In the MiddleWare!");
+    res.send("<h1>HOme Page</h1>");
 });
 
 app.listen(PORT);
