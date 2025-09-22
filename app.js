95eea350071fc26bdfbb20290e2cb5a2f4fc88b1
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2025-09-22 10:38:18 +0530

    feat: How to use next() to pass your request object to next middlware

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const http = require("http");
+
 const express = require("express");
+
 const PORT = 3000;
-const server = http.createServer();
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("In the MiddleWare!");
+    /* We have to tell that go to next middleware*/
+    next(); /* Allows request object to continue to next middleware */
+});
+
+app.use((req, res, next) => {
+    console.log("Another Middleware");
+});
+
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
     console.log(`Your server is running on port ${PORT}`);
