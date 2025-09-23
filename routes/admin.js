6c8c5a41e3af9b15ae39cf086846ae49cca73520
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2025-09-23 16:12:09 +0530

    Move route of '/' , '/add-product','/product' to shop.js and admin.js

---
+++ routes/admin.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.use("/add-product", (req, res, next) => {
+    res.send(
+        "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>ADD-PRODUCT</button></form>",
+    );
+});
+
+router.post("/product", (req, res, next) => {
+    console.log(req.body.title);
+    res.redirect("/");
+});
+
+/* In the end don't forget to EXPORT it */
+
+module.exports = router;
