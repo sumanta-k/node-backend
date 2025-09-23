06a4ee7e725618d76573fde9e2676a555d60d9fa
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2025-09-23 11:38:06 +0530

    Remove serving <h1> , custom response and console.log
    Add a form which has a path or action='product' and it was a post request' -m 'Add a middleware for handling /add-product and /product'

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 const http = require("http");
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const PORT = 3000;
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+/* extended : true --> this will work for ** nested OBJECT and ARRAY ** */
+/* extended : false ---> this will work for ** strings ** */
+
 app.use("/add-product", (req, res, next) => {
-    console.log("Another Middleware");
-    console.log(req.url);
-    res.setHeader("Connection", "keep-alive");
-    res.send("<h1>HI From Express.js</h1>");
+    res.send(
+        "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>ADD-PRODUCT</button></form>",
+    );
+});
+
+app.post("/product", (req, res, next) => {
+    console.log(req.body.title);
+    res.redirect("/");
 });
 
 app.use("/message", (req, res, next) => {
     console.log("inside the /message path");
     console.log(req.url);
-    res.send(` inside the path of ${req.url}`);
+    res.send(`<h1> inside the path of ${req.url}</h1>`);
 });
 
 app.use("/", (req, res, next) => {
-    console.log("In the MiddleWare!");
+    console.log("In the homePage MiddleWare!");
     res.send("<h1>HOme Page</h1>");
 });
 
