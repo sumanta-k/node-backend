6ff8624c42ec648c8fd3022a0d8f0023ef283589
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2025-09-23 16:14:20 +0530

    import the route of '/', '/add-product','/product' to app.js , instead of writing down whole respective middleware inside the app.js

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+/* importing files from local directory */
+const adminRoute = require("./routes/admin.js");
+const shopRoute = require("./routes/shop.js");
+
 const PORT = 3000;
 
 const app = express();
@@ -11,26 +15,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 /* extended : true --> this will work for ** nested OBJECT and ARRAY ** */
 /* extended : false ---> this will work for ** strings ** */
 
-app.use("/add-product", (req, res, next) => {
-    res.send(
-        "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>ADD-PRODUCT</button></form>",
-    );
-});
-
-app.post("/product", (req, res, next) => {
-    console.log(req.body.title);
-    res.redirect("/");
-});
-
-app.use("/message", (req, res, next) => {
+app.get("/message", (req, res, next) => {
     console.log("inside the /message path");
     console.log(req.url);
     res.send(`<h1> inside the path of ${req.url}</h1>`);
 });
 
-app.use("/", (req, res, next) => {
-    console.log("In the homePage MiddleWare!");
-    res.send("<h1>HOme Page</h1>");
-});
+/* placing which one comes first after which one  , does really matter , so be careful when placing */
+app.use(adminRoute);
+app.use(shopRoute);
 
 app.listen(PORT);
