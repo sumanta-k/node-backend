847a30562901d2b48b1ebe8df66cd25ef32dc254
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2025-09-24 21:19:42 +0530

    Add global module 'path' and serve add-product.html

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
-router.use("/add-product", (req, res, next) => {
-    res.send(
-        "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>ADD-PRODUCT</button></form>",
-    );
+/*  NOTE:     GET   /admin/add-product */
+
+router.get("/add-product", (req, res, next) => {
+    res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
+    // res.send(
+    //     "<form action='/admin/add-product' method='POST'><input type='text' name='title'><button type='submit'>ADD-PRODUCT</button></form>",
+    // );
 });
 
 router.post("/product", (req, res, next) => {
