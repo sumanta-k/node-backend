d3024d4de097e58c65cbe9eacc4f4dde0a681d4f
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2025-09-25 10:03:04 +0530

    import common root path and serve add-product page

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const path = require("path");
 
+const rootDir = require("../util/path.js");
+
 const router = express.Router();
 
 /*  NOTE:     GET   /admin/add-product */
 
 router.get("/add-product", (req, res, next) => {
-    res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
+    res.sendFile(path.join(rootDir, "views", "add-product.html"));
     // res.send(
     //     "<form action='/admin/add-product' method='POST'><input type='text' name='title'><button type='submit'>ADD-PRODUCT</button></form>",
     // );
