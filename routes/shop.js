070349fa718efee9cb0ad25bf79f42dbb960492d
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2025-09-25 10:02:09 +0530

    import common root path and use it to send the path for homepage

--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,10 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
+const rootDir = require("../util/path.js");
+
 router.get("/", (req, res, next) => {
-    res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
+    res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
