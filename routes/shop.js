ae0fab36e38a5da6bb83d5d251be2a11a835ccde
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2025-10-18 20:00:39 +0530

    FIX: The path of shop page

--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,13 @@ const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+    /*
     console.log("shop.js", adminData.products);
     res.sendFile(path.join(rootDir, "views", "shop.html"));
+    */
+   // it wasn't rendering the shop.pug , cause we had set it to shop.html in the above line
+   // so i had to use the .render() to render shop.pug file
+   res.render('shop');
 });
 
 module.exports = router;
