2d407cff42d494d6d8ad693926de772a595f2d0d
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2025-10-23 10:22:00 +0530

    Render shop.pug file in browser , for path '/' and send a object

--- routes/shop.js
+++ routes/shop.js
@@ -13,9 +13,9 @@ router.get("/", (req, res, next) => {
     console.log("shop.js", adminData.products);
     res.sendFile(path.join(rootDir, "views", "shop.html"));
     */
-   // it wasn't rendering the shop.pug , cause we had set it to shop.html in the above line
-   // so i had to use the .render() to render shop.pug file
-   res.render('shop');
+    // it wasn't rendering the shop.pug , cause we had set it to shop.html in the above line
+    // so i had to use the .render() to render shop.pug file
+    res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
