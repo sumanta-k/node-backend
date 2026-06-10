2dd1ccec4600a1e97327f6a35cf0e34408988f1c
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-06-10 09:32:26 +0530

    chore: fix indentation

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const http = require("https");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+app.use();
 const app = express();
 app.set("views", "views");
 
@@ -17,7 +18,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-	res.status(404).render("404.pug", { pageTitle: "Page Not Found" });
+    res.status(404).render("404.pug", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
